8d88c2a1187d353caca47a0dc64dee0412480bff
methpaul <paulciulavu2004@yahoo.com>
2026-04-12 15:33:42 +0300

    Realizare pagina loghin.

--- MainApplication/MainApplication/MainWindow.xaml.cs
+++ MainApplication/MainApplication/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Text;
+﻿/* * Proiect: MES Interface
+ * Modul: Gestiune Acces Utilizatori
+ * Autor: Ciulavu Paul
+ * Data Creării: 2026-04-11
+ * Descriere: Gestiune logica intre utilizatori
+ */
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +17,34 @@ using System.Windows.Shapes;
 
 namespace MainApplication
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string user = txtUsername.Text;
+            string pass = txtPassword.Password;
+
+            //TO DO: Apeleaza utilizatorii dintr-o baza de date
+
+            if (user == "admin" && pass == "1234")
+            {
+                MessageBox.Show("Acces Grantat: ADMINISTRATOR", "Sistem Industrial", MessageBoxButton.OK, MessageBoxImage.Information);
+                txtPassword.Clear();
+            }
+            else if (user == "operator" && pass == "0000")
+            {
+                MessageBox.Show("Acces Grantat: OPERATOR", "Sistem Industrial", MessageBoxButton.OK, MessageBoxImage.Information);
+                txtPassword.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Eroare: Utilizator sau parolă incorecte", "Acces Respins", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
